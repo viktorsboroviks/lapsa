a29b5cf6b6ff52e356a42a626066c7a4804ef4ff
Viktors Boroviks <vboroviks@inbox.lv>
2025-07-07 10:12:08 +0300

    iestaade updated; examples fixed

--- examples/max_array_adaptive_cooling.cpp
+++ examples/max_array_adaptive_cooling.cpp
@@ -17,16 +17,14 @@ private:
         // if energy not calculated, do it now and store the result
         if (!_evaluated) {
             assert(_data.size() != 0);
-            _value  = std::accumulate(_data.begin(), _data.end(), 0.0);
-            _energy = 1.0 / _value;
+            _energy = -std::accumulate(_data.begin(), _data.end(), 0.0);
         }
         _evaluated = true;
     }
 
 public:
-    explicit MyState(lapsa::Settings& in_settings) :
-        State(in_settings),
-        _data(iestaade::size_t_from_json(CONFIG_PATH, "state/data_size"))
+    MyState() :
+        _data(iestaade::from_json<size_t>(CONFIG_PATH, "state/data_size"))
     {
     }
 
@@ -34,12 +32,6 @@ public:
     {
         _evaluate();
         return _energy;
-    }
-
-    double get_value() override
-    {
-        _evaluate();
-        return _value;
     }
 
     void randomize() override
@@ -64,47 +56,53 @@ public:
 
 int main()
 {
-    lapsa::Settings s(CONFIG_PATH, "lapsa");
-    lapsa::StateMachine<MyState> sm{s};
+    lapsa::StateMachine<MyState> sm(CONFIG_PATH, "lapsa");
+    // clang-format off
     sm.init_functions = {
-            lapsa::init_log<MyState>,
-            lapsa::randomize_state<MyState>,
+            lapsa::init_log                     <MyState>,
+            lapsa::randomize_state              <MyState>,
     };
     sm.init_loop_functions = {
             // update state
-            lapsa::propose_new_state<MyState>,
-            lapsa::init_t_history<MyState>,
-            lapsa::init_t_select_max<MyState>,
-            lapsa::init_progress<MyState>,
+            lapsa::run_i_inc                    <MyState>,
+            lapsa::propose_new_state            <MyState>,
+            lapsa::generate_init_t_candidates   <MyState>,
+            lapsa::select_init_t_as_max         <MyState>,
             // decide to proceed
-            lapsa::decide_init_done<MyState>,
+            lapsa::decide_init_done             <MyState>,
+    };
+    sm.run_functions = {
+            lapsa::progress_init_run_loop       <MyState>,
     };
     sm.run_loop_functions = {
             // log
-            lapsa::update_log<MyState>,
+            lapsa::update_log                   <MyState>,
             // progress bar
-            lapsa::progress_text_reset<MyState>,
-            lapsa::progress_text_add_total<MyState>,
-            lapsa::progress_text_add_pct<MyState>,
-            lapsa::progress_text_add_eta<MyState>,
-            lapsa::progress_text_add_t<MyState>,
-            lapsa::progress_text_add_e<MyState>,
-            lapsa::progress_text_add_v<MyState>,
-            lapsa::progress_print<MyState>,
+            lapsa::progress_text_reset          <MyState>,
+            lapsa::progress_text_add_total      <MyState>,
+            lapsa::progress_text_add_pct        <MyState>,
+            lapsa::progress_text_add_eta        <MyState>,
+            lapsa::progress_text_add_t          <MyState>,
+            lapsa::progress_text_add_e          <MyState>,
+            lapsa::progress_print               <MyState>,
             // decide to proceed and begin next run
-            lapsa::decide_run_done<MyState>,
+            lapsa::decide_run_done              <MyState>,
             // update state
-            lapsa::propose_new_state<MyState>,
-            lapsa::e_history_update<MyState>,
-            lapsa::decide_cool_sma<MyState>,
-            lapsa::cool_at_rate<MyState>,
-            lapsa::update_state<MyState>,
+            lapsa::propose_new_state            <MyState>,
+            lapsa::e_history_update             <MyState>,
+            lapsa::decide_cool_sma              <MyState>,
+            lapsa::cool_at_rate                 <MyState>,
+            lapsa::run_i_inc                    <MyState>,
+            lapsa::update_state                 <MyState>,
+            lapsa::reset_state_evaluation       <MyState>,
+            // update state
     };
     sm.finalize_functions = {
-            lapsa::progress_text_reset<MyState>,
-            lapsa::stats_print<MyState>,
-            lapsa::stats_create_file<MyState>,
+            lapsa::progress_text_reset          <MyState>,
+            lapsa::print_stats                  <MyState>,
+            lapsa::create_stats_file            <MyState>,
     };
+    // clang-format on
 
     sm.run();
     return 0;
--- examples/max_array_cooling_schedule.cpp
+++ examples/max_array_cooling_schedule.cpp
@@ -18,7 +18,7 @@ private:
         // if energy not calculated, do it now and store the result
         if (!_evaluated) {
             assert(_data.size() != 0);
-            _energy  = 1.0 / std::accumulate(_data.begin(), _data.end(), 0.0);
+            _energy = -std::accumulate(_data.begin(), _data.end(), 0.0);
         }
         _evaluated = true;
     }
@@ -53,44 +53,51 @@ public:
 
 int main()
 {
-    lapsa::StateMachine<MyState> sm{CONFIG_PATH, "lapsa"};
+    lapsa::StateMachine<MyState> sm(CONFIG_PATH, "lapsa");
+    // clang-format off
     sm.init_functions = {
-            lapsa::init_log<MyState>,
-            lapsa::randomize_state<MyState>,
+            lapsa::init_log                     <MyState>,
+            lapsa::randomize_state              <MyState>,
     };
     sm.init_loop_functions = {
             // update state
-            lapsa::run_i_inc<MyState>,
-            lapsa::propose_new_state<MyState>,
-            lapsa::generate_init_t_candidates<MyState>,
-            lapsa::select_init_t_as_max<MyState>,
+            lapsa::run_i_inc                    <MyState>,
+            lapsa::propose_new_state            <MyState>,
+            lapsa::generate_init_t_candidates   <MyState>,
+            lapsa::select_init_t_as_max         <MyState>,
             // decide to proceed
-            lapsa::decide_init_done<MyState>,
+            lapsa::decide_init_done             <MyState>,
+    };
+    sm.run_functions = {
+            lapsa::progress_init_run_loop       <MyState>,
     };
     sm.run_loop_functions = {
             // log
-            lapsa::update_log<MyState>,
+            lapsa::update_log                   <MyState>,
             // progress bar
-            lapsa::progress_text_reset<MyState>,
-            lapsa::progress_text_add_total<MyState>,
-            lapsa::progress_text_add_pct<MyState>,
-            lapsa::progress_text_add_eta<MyState>,
-            lapsa::progress_text_add_t<MyState>,
-            lapsa::progress_text_add_e<MyState>,
-            lapsa::progress_print<MyState>,
+            lapsa::progress_text_reset          <MyState>,
+            lapsa::progress_text_add_total      <MyState>,
+            lapsa::progress_text_add_pct        <MyState>,
+            lapsa::progress_text_add_eta        <MyState>,
+            lapsa::progress_text_add_t          <MyState>,
+            lapsa::progress_text_add_e          <MyState>,
+            lapsa::progress_print               <MyState>,
             // decide to proceed and begin next run
-            lapsa::decide_run_done<MyState>,
+            lapsa::decide_run_done              <MyState>,
             // update state
-            lapsa::propose_new_state<MyState>,
-            lapsa::do_cool_set<MyState>,
-            lapsa::cool_at_rate<MyState>,
-            lapsa::update_state<MyState>,
+            lapsa::propose_new_state            <MyState>,
+            lapsa::do_cool_set                  <MyState>,
+            lapsa::cool_at_rate                 <MyState>,
+            lapsa::run_i_inc                    <MyState>,
+            lapsa::update_state                 <MyState>,
+            lapsa::reset_state_evaluation       <MyState>,
     };
     sm.finalize_functions = {
-            lapsa::progress_text_reset<MyState>,
-            lapsa::print_stats<MyState>,
-            lapsa::create_stats_file<MyState>,
+            lapsa::progress_text_reset          <MyState>,
+            lapsa::print_stats                  <MyState>,
+            lapsa::create_stats_file            <MyState>,
     };
+    // clang-format on
     sm.run();
     return 0;
 }
